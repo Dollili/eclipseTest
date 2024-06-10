e910a8dd63ef3c87b1c42dee6ecd458c1c2c18ba
Dollili <ad____da@naver.com>
2024-06-10 16:15:10 +0900

    문법교정

--- Test1/src/test/testfile.java
+++ Test1/src/test/testfile.java
@@ -4,11 +4,11 @@ public class testfile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=1;
-		int b=2;
-		int c = a+b;
-		System.out.println(c);		
-		
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println(c);
+
 	}
 
 }
