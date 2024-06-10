732cffdb9ca331652910e8798a08b392f78419ff
Dollili <ad____da@naver.com>
2024-06-10 16:14:02 +0900

    testtest

--- Test1/src/test/testfile.java
+++ Test1/src/test/testfile.java
@@ -4,7 +4,11 @@ public class testfile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a;
+		int a=1;
+		int b=2;
+		int c = a+b;
+		System.out.println(c);		
+		
 	}
 
 }
